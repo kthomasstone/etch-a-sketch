0e9c2503a3885010079735a696847b5404f42d87
kthomasstone <22228480+kthomasstone@users.noreply.github.com>
2023-09-11 16:15:39 -0500

    adjust controls section html and css.

--- script.js
+++ script.js
@@ -21,15 +21,17 @@ function setupGrid(size) {
 }
 setupGrid(16);
 
-// UNICORN PUKE EFFECT
-function getRandomColor() {
-  // Generate a random color in hexadecimal format
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-
 /* HOVER EFFECTS */
 // mouseover
 gridContainer.addEventListener("mouseover", function(event) {
+  if (unicornMode == true) {
+    if (event.target.classList.contains("grid-cell")) {
+      event.target.style.backgroundColor = getRandomColor();
+    } else if (event.target.classList.contains("grid-cell")) {
+      event.target.classList.toggle("grid-cell-active", true);
+    }
+  }
+
   if (event.target.classList.contains("grid-cell")) {
     event.target.classList.toggle("grid-cell-active", true);
   }
@@ -50,3 +52,27 @@ function createNewGrid() {
   );
   setupGrid(gridCellsPerRow);
 }
+
+// UNICORN MODE
+let unicornMode = false;
+const unicornModeButton = document.getElementById("unicorn");
+
+// listen for unicorn puke button focus
+unicornModeButton.addEventListener("focus", () => {
+  unicornMode = true;
+  console.log("unicorn is focused");
+});
+// listen for unicorn puke button blur
+unicornModeButton.addEventListener("blur", () => {
+  unicornMode = false;
+  console.log("unicorn has lost its focus");
+});
+
+// create function to generate random color
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+// update variable with random color
+
+// change background color grid-cell-active class to the random hex code
